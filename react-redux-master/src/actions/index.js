72d591195077ba51aab2f7c404dda41b982e1db7
Nikki Gowan <nikkigowan2@gmail.com>
2023-12-29 15:39:10 -0500

    Redux with page that tracks number of times button is clicked

--- react-redux-master/src/actions/index.js
+++ react-redux-master/src/actions/index.js
@@ -1,1 +1,9 @@
-//actions
+//increase val each time button is clicked
+const increment = (val) => {
+    return {
+        type : 'INCREMENT',
+        inc : val
+    }
+}
+
+export default increment;
